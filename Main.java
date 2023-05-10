54ec089953d5212de5d759156d22ee0b7dd26753
AliakseiShylovich <131357751+AliakseiShylovich@users.noreply.github.com>
2023-05-10 14:38:50 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        for (a = 1; a<=10; a = a + 1)
+            System.out.println( a + " *  5 = " + a*5 );
+    }
+}
